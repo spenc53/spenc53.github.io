078d9c2135bb267338e2792b5a1f9ecaa9f370b9
Spencer Seeger <seeger.spencer@gmail.com>
2020-02-08 13:49:02 -0600

    Updates

--- precache-manifest.ff6ee47e44e9103a153591baa19eae81.js
+++ precache-manifest.ff6ee47e44e9103a153591baa19eae81.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a76b74440c5226dcc9d28e3eb5dd8f0",
+    "revision": "524f1312ed679043e4563d3ad248dbad",
     "url": "/index.html"
   },
   {
-    "revision": "d2eb1694417e830165e9",
-    "url": "/static/js/2.d16696d2.chunk.js"
+    "revision": "e01a9ef2ab12a97f2063",
+    "url": "/static/js/2.921d8611.chunk.js"
   },
   {
-    "revision": "0874344ce9194702a07c",
-    "url": "/static/js/main.88cd8bd6.chunk.js"
+    "revision": "a17a1e367b1ec907efcc",
+    "url": "/static/js/main.fb255159.chunk.js"
   },
   {
     "revision": "b78327512a92b527706c",
