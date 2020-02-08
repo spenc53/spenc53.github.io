d5d7d46c07f4f89cc62225d620a09c3ef520e071
Spencer Seeger <seeger.spencer@gmail.com>
2020-02-08 15:08:02 -0600

    redoing home page paragraphs

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -33,7 +33,7 @@ class HomePage extends Component {
                             <Heading margin={{ top: "large", bottom: "none" }} level="2">About Me</Heading>
                             <Paragraph fill>
                                 <Tab/>
-                                Hi, I'm Spencer Seeger! I'm a software engineer and I love to code, learn, and build things! If I think of an idea I think would be fun to try, I sit down and try it out! 
+                                Hi, I'm Spencer Seeger! I'm a software engineer and I love to code, learn, and build things! If I think of an idea I think would be fun to try, I sit down and try it out. Some of my hobbies and interests are: gaming (my current favorite game is Beat Saber), cooking, and coding.
                             </Paragraph>
                             <Paragraph fill>
                                 <Tab />Currently, I work at Sam's Club Technology helping develop a chatbot platform. As part of this project, I have helped develop a platform which Sam's Club uses to in both the clubs and on their online site. Online, the chat bot is used to help resolve members questions. The chat bot can asnwer simple questions or it can help a member start the return process of an item. Another use for this chat bot platform is "Ask Sam". "Ask Sam" is a Siri-like digital assistant for associates in the club. Some things Ask Sam is able to do is help associates know who is working next, how to prepare food, and check product avaiablity.
